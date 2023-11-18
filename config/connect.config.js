0fa6dd333d8ad6d8080a5218b97360d41ed2810f
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-11-18 23:39:00 +0700

    Update connect.config.js

--- config/connect.config.js
+++ config/connect.config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const connect = mysql.createPool({
   host: "103.169.35.190",
   user: "ecommerce",
-  password: "ecommerce123",
+  password: "~UpQcl.gpV&}",
   port: 3306,
   database: "ecommerce"
 });
