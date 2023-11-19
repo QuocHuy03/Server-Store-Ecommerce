6107c6df3b83b3c5d1b683dca3fb681823df712c
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-11-19 10:32:39 +0700

    Update connect.config.js

--- config/connect.config.js
+++ config/connect.config.js
@@ -1,23 +1,15 @@
 const mysql = require("mysql2/promise");
 
-const connect = async () => {
-  try {
-    const pool = mysql.createPool({
-      host: "103.169.35.190",
-      user: "ecommerce",
-      password: "~UpQcl.gpV&}",
-      port: 3306,
-      database: "ecommerce"
-    });
+const connect = mysql.createPool({
+  host: "103.169.35.190",
+  user: "ecommerce",
+  password: "~UpQcl.gpV&}",
+  port: 3306,
+  database: "ecommerce"
+});
 
-    // Kiểm tra kết nối
-    await pool.getConnection();
-    console.log("Connect successful");
-    return pool;
-  } catch (error) {
-    console.error("Connect failed:", error.message);
-    throw error;
-  }
-};
+if (!connect) {
+  console.log("Connect Failed");
+}
 
 module.exports = connect;
